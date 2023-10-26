23bf27bc67067db5fb2a50e0470ad750f8dfe5e7
pemmarilaine <127546927+pemmarilaine@users.noreply.github.com>
2023-10-26 14:22:25 -0300

    main

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 alert ('ola mundo');
+function tocaSom (seletorAudio) {
+    const elemento = document. querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio'){
+        elemento.play();
+    }
+    else{
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
+}
+
+const listaDeTeclas= document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador< listaDeTeclas.length; contador++){
+
+    const tecla = listasDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = '#som_${instrumento}'; //template string
+
+    tecla.onclick = function () {
+        tocarSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento){
+
+        if (evento.code === 'Space')
+    }
